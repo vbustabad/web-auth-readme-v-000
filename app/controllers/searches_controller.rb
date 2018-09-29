bc985188bf02f7e17032516e91d05f000c50d70c
Veronica Bustabad <vbustabad@gmail.com>
2018-09-29 22:38:26 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,6 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-
     client_id = ENV['FOURSQUARE_CLIENT_ID']
     client_secret = ENV['FOURSQUARE_SECRET']
 
@@ -27,5 +26,13 @@ class SearchesController < ApplicationController
     rescue Faraday::TimeoutError
       @error = "There was a timeout. Please try again."
       render 'search'
+  end
+
+  def friends
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = session[:token]
+      req.params['v'] = '20160201'
+    end
+    @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
   end
 end
